74a12502b3477c95dab4a78eca5c25b24f07c883
dmevola <dmevola@gmail.com>
2022-01-08 12:54:37 -0500

    added logic to change color by hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,34 @@
-var currentDay = function() {
-    $("#currentDay").text(moment().format('MMMM Do YYYY'))
+
+//global variables
+var button = $(".saveBtn")
+
+// add current day to top of page
+
+
+$(document).ready(function () {
+
+ var date = moment().format('dddd MMMM Do');
+ $('#currentDay').text(date);
+
+function currentTime() {
+    var current = moment().hours()
+    var time = $('.time-block');
+
+
+    time.each(function () {
+        var hour = parseInt($(this).attr('id'))
+        console.log(hour)
+        
+        if (hour === current) {
+            $(this).children('.col-sm-10').attr('class', 'present col-sm-10 description')
+        } else if (current > hour) {
+            $(this).children('.col-sm-10').attr('class', 'past col-sm-10 description')
+        } else {
+            $(this).children('.col-sm-10').attr('class', 'future col-sm-10 description')
+        }
+    })
 }
 
-currentDay ();
+currentTime()
+
+});
